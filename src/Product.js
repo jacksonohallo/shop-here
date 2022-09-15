6aa50d58e83cb2982e7d4404302510adb937cde6
jacksonohallo <jacksonohallo@gmail.com>
2022-09-15 13:07:53 +0300

    add displaying products

--- src/Product.js
+++ src/Product.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Product = ({id, title, image, ratings}) => {
+const Product = ({id, title, image,price,ratings}) => {
   return (
       <div className='product'>
           <p>{title}</p>
+          <p className='product__listing'></p>
+         <small>$</small>
+        <strong>{price}</strong>
         
 
 
