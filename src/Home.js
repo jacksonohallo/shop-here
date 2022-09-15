509df48a1da4fde6d4aabf36baeff195ecefa497
jacksonohallo <jacksonohallo@gmail.com>
2022-09-15 12:54:15 +0300

    add product component to handle products

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import Product from "./Product";
+import "./Home.css";
+
 const Home = () => {
   return (
-      <div className='home'>    
-    <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-    alt="banner"
-    className="home__image" />'
-               
-    </div>
-  )
-}
+    <div className="home">
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt="banner"
+        className="home__image"
+      />
 
-export default Home
+      <Product
+        id={12312}
+        title="GSM Fitness Hex Dumbbell, 7.5 kgs, Pack of 2, Rubber Coatedo"
+        price={4051.0}
+        rating={3}
+        image="https://images-na.ssl-images-amazon.com/images/I/417wp10uSeL.jpg"
+      />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Product = ({id, title, image, ratings}) => {
+  return (
+      <div className='product'>
+          <p>{title}</p>
+        
+
+
+        </div>
+  )
+}
+
+export default Product
