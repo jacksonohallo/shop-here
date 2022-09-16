ac77020456f4ba57f7db657cb17d3e18b8636176
jacksonohallo <jacksonohallo@gmail.com>
2022-09-16 13:55:34 +0300

    add checkout product to handle sales

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,29 +1,44 @@
-import React from 'react'
-import { useStateValue } from './StateProvider'
-import './Checkout.css'
+import React from "react";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Checkout.css";
 
 const Checkout = () => {
-    const [{ basket }] = useStateValue();
+  const [{ basket }] = useStateValue();
 
   return (
-      <div className='checkout'>
-          <img alt="Ads"
-            className="checkout__ad"
-            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-          />
-          {basket?.length === 0 ? (
-              <div>
-                  <h2> Your Shopping basket is empty</h2>
-        <p>You have no items in your basked. To buy one or more items, click "Add to basket" next to the item.
-                    </p>  
-              </div> 
-          ):(
-                  <div>
-                <h2>Your Shopping Basket</h2>
-               </div>   
-         )};          
-      </div>
-  )
-}
+    <div className="checkout">
+      <img
+        alt="Ads"
+        className="checkout__ad"
+        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+      />
+      {basket?.length === 0 ? (
+        <div>
+          <h2> Your Shopping basket is empty</h2>
+          <p>
+            You have no items in your basket. To buy one or more items, click
+            "Add to basket" next to the item.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
 
-export default Checkout
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
+      )}
+      ;
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CheckoutProduct({ id, title, image, price, rating}) {
+  return (
+      <div>
+          
+
+          
+    </div>
+  )
+}
+
+export default CheckoutProduct
