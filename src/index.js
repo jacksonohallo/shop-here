753ee64b6f603518ed88d50a8ed597b416a04164
jacksonohallo <jacksonohallo@gmail.com>
2022-09-16 10:56:35 +0300

    implementing add_to_basket/remove with reducer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StateProvider } from './StateProvider';
+import reducer,{ initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <StateProvider initialState ={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+   
+    
     </Router>
+
 
 );
 
