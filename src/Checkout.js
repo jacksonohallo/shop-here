4faae30f10ba2a06554486d9046817140ff77e16
jacksonohallo <jacksonohallo@gmail.com>
2022-09-16 12:21:30 +0300

    add styling to checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Checkout.css'
+import { useStateValue } from './StateProvider'
 
 const Checkout = () => {
+    const [{ basket }] = useStateValue();
+
   return (
-    <div>Checkout</div>
+      <div className="checkout">
+           <img alt="Ads"
+                    className="checkout__ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                />
+        
+    </div>
   )
 }
 
