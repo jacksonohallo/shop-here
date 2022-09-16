dbf99b57113695032e2b3b0196ffda29d96d7917
jacksonohallo <jacksonohallo@gmail.com>
2022-09-16 17:54:10 +0300

    add functionality preventDefault for login page

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  
+
+  const login = (event) => {
+    event.preventDefault();
+  };
+
+
+  const register = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div>Login</div>
-  )
+    
+  <div className="login">
+            <Link to="/">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+                    className="login__logo"
+                    alt="Logo" />
+            </Link>
+            <div className="login__container">
+                <h1>Sign In</h1>
+                <form>
+                    <h5>Email</h5>
+                    <input type="email" onChange={setUserName} />
+                    <h5>Password</h5>
+                    <input type="password" />
+                    <Link to="/">
+                        <button
+                            className="login__signInButton"
+                            type="submit"
+                        >Sign in</button>
+                    </Link>
+                </form>
+                <p>
+                    By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
+                </p>
+                <button
+                    className="login__registerButton"
+                    onClick={register}
+                >Create your Amazon account</button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Login
+
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,18 @@
-const firebaseConfig = {
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+
   apiKey: "AIzaSyATkJ6YkNQSU9EwP12omlHYhZbo1qtAEqI",
   authDomain: "shop-yangu-c81c9.firebaseapp.com",
   projectId: "shop-yangu-c81c9",
   storageBucket: "shop-yangu-c81c9.appspot.com",
   messagingSenderId: "911780913595",
   appId: "1:911780913595:web:21b340536adea29089649e",
-  mea
+  measurementId: "G-E4CQ3V4XFB"
+})
+
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export {db, auth}
