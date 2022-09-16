45380d501194b52ec3a0292f7c13f73def6227e9
jacksonohallo <jacksonohallo@gmail.com>
2022-09-16 13:28:34 +0300

    add message to checkout box

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import './Checkout.css'
 import { useStateValue } from './StateProvider'
+import './Checkout.css'
 
 const Checkout = () => {
     const [{ basket }] = useStateValue();
 
   return (
-      <div className="checkout">
-           <img alt="Ads"
-                    className="checkout__ad"
-                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                />
-        
-    </div>
+      <div className='checkout'>
+          <img alt="Ads"
+            className="checkout__ad"
+            src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          />
+          {basket?.length === 0 ? (
+              <div>
+                  <h2> Your Shopping basket is empty</h2>
+        <p>You have no items in your basked. To buy one or more items, click "Add to basket" next to the item.
+                    </p>  
+              </div> 
+          ):(
+                  <div>
+                <h2>Your Shopping Basket</h2>
+               </div>   
+         )};          
+      </div>
   )
 }
 
